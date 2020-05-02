c66d8af58c4b1a521ca7d2af91940f788999c871
“mamohamed252” <ma.mohamed252@gmail.com>
2020-05-02 15:29:56 -0500

    App lesson 4

---
+++ SgEx/src/main/java/com/mycompany/sgex/App.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sgex;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class App {
+    public static void main(String[] args) {
+        int count = 19;
+        changeMyValue(count);
+        System.out.println("Count = " + count);
+        Person myPerson = new Person();
+        myPerson.setAge(24);
+        myPerson.SetName("Joe");
+        System.out.println("Age = " + myPerson.getAge());
+        System.out.println("Name = " + myPerson.getName());
+        
+        changeMyName(myPerson);
+        
+        System.out.println("Age = " + myPerson.getAge());
+        System.out.println("Name = " + myPerson.getName());
+        
+        
+        }
+    public static void changeMyValue(int value) {
+        value = 21;
+        
+    }
+    
+    public static void changeMyName(Person person) {
+        person.SetName("Steve");
+        
+        
+        
+        
+    }
+}
---
+++ SgEx/src/main/java/com/mycompany/sgex/Person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sgex;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Person {
+    private int age;
+    private String name;
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void SetName(String name) {
+        this.name = name;
+    }
+}
