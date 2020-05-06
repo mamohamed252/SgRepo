28a80e382c9efda645753e0f1c7744b6474c92d5
mamohamed252 <ma.mohamed252@gmail.com>
2020-05-05 19:40:16 -0500

    Lesson 7

---
+++ SgEx/src/main/java/com/mycompany/sgex/module1/PracticeProblems.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sgex.module1;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class PracticeProblems {
+
+  
+
+}
