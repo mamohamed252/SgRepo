6a300f210fc402113d5e98011d7e1553d62820d2
“mamohamed252” <ma.mohamed252@gmail.com>
2020-05-07 00:48:34 -0500

    Practice

--- SgEx/src/main/java/com/mycompany/sgex/module1/PracticeProblems.java
+++ SgEx/src/main/java/com/mycompany/sgex/module1/PracticeProblems.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.sgex.module1;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Mohamed
@@ -17,6 +19,35 @@ public class PracticeProblems {
         int num2 = 6;
         System.out.println("Total is: " + add(num, num2));
         System.out.println("What is my name: " + add(name, name));
+        print();
+        System.out.println("My name is: " + print2());
+        
+        // first way to intialize array
+        int[] array1 = {3, 2, 1, 0};
+        array1[0]= 1;
+        array1[1] = 2;
+        array1[2] = 3;
+        array1[3] = 4;
+        // second way
+        int[] array2 = new int[6];
+        
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        // how to modify array
+        array1 = new int[4];
+        array1[2] = 3;
+        array1[3] = 4;
+        System.out.println(Arrays.toString(array1));
+        
+        // multi-dem array
+        int[][] array3 = new int[2][2];
+        System.out.println(Arrays.deepToString(array3));
+        // how to iterrate with array
+        for (int i = 0; i < array1.length; i++){
+            System.out.println(array1[i]);
+            
+        }
+        
     }
 
     public static int add(int numberAdded, int numberAdded2) {
@@ -28,4 +59,13 @@ public class PracticeProblems {
         String fullName = name + " 'Test' " + name2;
         return fullName;
     }
+    public static void print(){
+        System.out.println("My name Jeff");
+    }
+    public static String print2(){
+        String name = "Jon";
+        System.out.println("My name Ahmed");
+        return name;
+    }
+    
 }
