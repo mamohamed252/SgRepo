c2c3d43c86c9087fdbbe30e1929e10925620c8cc
“mamohamed252” <ma.mohamed252@gmail.com>
2020-05-06 23:56:13 -0500

    Practice

--- SgEx/src/main/java/com/mycompany/sgex/module1/PracticeProblems.java
+++ SgEx/src/main/java/com/mycompany/sgex/module1/PracticeProblems.java
@@ -11,6 +11,21 @@ package com.mycompany.sgex.module1;
  */
 public class PracticeProblems {
 
-  
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "Mohamed";
+        int num2 = 6;
+        System.out.println("Total is: " + add(num, num2));
+        System.out.println("What is my name: " + add(name, name));
+    }
 
+    public static int add(int numberAdded, int numberAdded2) {
+        int sum = numberAdded + numberAdded2;
+        return sum;
+    }
+
+    public static String add(String name, String name2) {
+        String fullName = name + " 'Test' " + name2;
+        return fullName;
+    }
 }
