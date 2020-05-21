4a8b6741e9d5cb29d28fcee751898ab22c0ae3d4
“mamohamed252” <ma.mohamed252@gmail.com>
2020-05-21 18:39:08 -0500

    Created Service Layer

--- Module 2 finished/ClassRoster/src/main/java/com/mycompany/classroster/dao/ClassRosterPersistenceException.java
+++ Module 2 finished/ClassRoster/src/main/java/com/mycompany/classroster/dao/ClassRosterPersistenceException.java
@@ -9,11 +9,11 @@ package com.mycompany.classroster.dao;
  *
  * @author Mohamed
  */
-public class ClassRosterDaoException extends Exception{
-    public ClassRosterDaoException(String message){
+public class ClassRosterPersistenceException extends Exception{
+    public ClassRosterPersistenceException(String message){
         super(message);
     }
-    public ClassRosterDaoException(String message, Throwable cause){
+    public ClassRosterPersistenceException(String message, Throwable cause){
         super(message, cause);
     }
 }
