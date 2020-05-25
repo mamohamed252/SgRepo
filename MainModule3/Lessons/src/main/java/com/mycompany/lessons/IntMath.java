367d6bdcdcc3a9128f07729c3f7a02f08355cf70
mamohamed252 <ma.mohamed252@gmail.com>
2020-05-24 23:48:20 -0500

    Date/Time API

---
+++ MainModule3/Lessons/src/main/java/com/mycompany/lessons/IntMath.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lessons;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class IntMath {
+    public int calculate(MathOperator operator, int operand1, int operand2){
+        switch(operator){
+            case PLUS:
+                return operand1 + operand2;
+            case MINUS:
+                return operand1 - operand2;
+            case MULTIPLY:
+                return operand1 * operand2;
+            case DIVIDE:
+                return operand1 / operand2;
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+    
+}
