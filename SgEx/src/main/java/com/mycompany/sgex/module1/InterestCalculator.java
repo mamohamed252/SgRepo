0c0089c690cf3d954f2ee1af9f4a21d9721abcf1
mamohamed252 <ma.mohamed252@gmail.com>
2020-09-05 18:21:37 -0500

    finished interest calc

--- SgEx/src/main/java/com/mycompany/sgex/module1/InterestCalculator.java
+++ SgEx/src/main/java/com/mycompany/sgex/module1/InterestCalculator.java
@@ -13,11 +13,49 @@ import java.util.Scanner;
  */
 public class InterestCalculator {
     public static void main(String[] args) {
-       int int1;
+       int howMuch;
+       int howManyYears;
+       Double annualGrowthPercent;
+              
     Scanner sc = new Scanner(System.in);
     String input;
-        System.out.println("How much do you want to invest?");
-    //hello
+        System.out.print("How much do you want to invest?");
+    input = sc.nextLine();
+        System.out.println(input);
+    howMuch = Integer.parseInt(input);
+        System.out.print("How many years are investing?");
+        input = sc.nextLine();
+    howManyYears = Integer.parseInt(input);
+        System.out.println(input);
+        System.out.print("What is the annual interest rate % growth?");
+        input = sc.nextLine();
+        annualGrowthPercent = Double.parseDouble(input);
+        System.out.println(input);
+        System.out.println("Calculating...");
+        year(howManyYears, howMuch, annualGrowthPercent);
+        
+    }
+    public static void year(int howManyYears, int howMuch, double annualGrowthPercent){
+        double beganWith = howMuch;
+        double endedWith= beganWith * (1 + (annualGrowthPercent / 100) );
+        double earned = (beganWith * annualGrowthPercent/100) ;
+       
+
+        
+        
+        for (int i = 0; i<= howManyYears; i++){
+     
+            if (i <= howManyYears)
+                earned = beganWith * (annualGrowthPercent/100) ;
+                System.out.println("Year " + i + ":");
+                System.out.println("Began with " + beganWith);
+                System.out.println("Earned " + (earned));
+                System.out.println("Ended with " + endedWith + "\n");
+                beganWith += earned;
+                endedWith += earned;
+            
+        }
+        
     }
     
     
