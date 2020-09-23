5b34c27e1de71799fb14839448d265414daaf80e
“mamohamed252” <ma.mohamed252@gmail.com>
2020-09-22 20:51:23 -0500

    Vending Machine complete. No Audit created

--- VendingMachineTwo/src/main/java/com/mycompany/vendingmachinetwo/VendingMachineApp.java
+++ VendingMachineTwo/src/main/java/com/mycompany/vendingmachinetwo/VendingMachineApp.java
@@ -12,6 +12,8 @@ import com.mycompany.vendingmachinetwo.UI.UserIO;
 import com.mycompany.vendingmachinetwo.UI.UserIOConsoleImpl;
 import com.mycompany.vendingmachinetwo.UI.VendingMachineView;
 import com.mycompany.vendingmachinetwo.controller.VendingMachineController;
+import com.mycompany.vendingmachinetwo.service.VendingMachineServiceLayer;
+import com.mycompany.vendingmachinetwo.service.VendingMachineServiceLayerImpl;
 
 /**
  *
@@ -19,11 +21,12 @@ import com.mycompany.vendingmachinetwo.controller.VendingMachineController;
  */
 public class VendingMachineApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         UserIO myIo = new UserIOConsoleImpl();
         VendingMachineView myView = new VendingMachineView(myIo);
         VendingMachineDAO myDao = new VendingMachineDAOFileImpl();
-        VendingMachineController controller = new VendingMachineController(myDao, myView);
+        VendingMachineServiceLayer myService = new VendingMachineServiceLayerImpl(myDao);
+        VendingMachineController controller = new VendingMachineController(myService, myView);
         controller.run();
     }
 
